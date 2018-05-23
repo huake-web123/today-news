d5aa32c1bb4eb3aadb0ff078f090b17fb4a5f518
smilehanxiao <413169485@qq.com>
2018-05-24 00:50:42 +0800

    获取数据

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <div class="news">
-      <div><img></div>
-      <div>
-        <!--<div>{{title}}</div>-->
-        <div></div>
+      <img>
+      <div class="content">
+        <div class="content-title">看见纽约高中的中文试卷，我可能是个假中国人</div>
+        <div>
+          <span>其他</span>
+          <span>评论</span>
+        </div>
       </div>
     </div>
     <div class="news"></div>
@@ -23,6 +26,16 @@ export default {
     return {
       msg: 'home'
     }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      this.$ajax({
+        method: 'get',
+        url: 'http://restaurant.yijiahotel.shop/v1/news/1/10'
+      }).then((res) => {
+        console.log(res)
+      })
+    })
   }
 }
 </script>
