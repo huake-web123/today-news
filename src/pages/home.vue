dc7a7f2003ec8203aa7ba6a7600a3c9256d7254e
smilehanxiao <413169485@qq.com>
2018-05-31 00:26:21 +0800

    关于过滤器以及时间戳的写法

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,21 +1,17 @@
 <template>
   <div>
-    <div class="news">
-      <img>
+    <div v-for="item in newsArr" :key="item.id" class="news">
+      <img v-bind:src="item.thumb_url">
       <div class="content">
-        <div class="content-title">看见纽约高中的中文试卷，我可能是个假中国人</div>
-        <div>
-          <span>其他</span>
-          <span>评论</span>
+        <div class="content-title">{{item.title}}</div>
+        <div class="content-foot">
+          <span class="txt-type">{{item.category | categoryFilters}}</span>
+          <span class="comments">{{item.comments_counts}}&nbsp;评论</span>
+          <span class="date">{{item.created_at | dateFilters}}</span>
         </div>
       </div>
     </div>
-    <div class="news"></div>
-    <div class="news"></div>
-    <div class="news"></div>
-    <div class="news"></div>
-    <div class="news"></div>
-    {{msg}}
+    <!--{{msg}}-->
   </div>
 </template>
 
@@ -24,7 +20,41 @@ export default {
   name: 'home',
   data () {
     return {
-      msg: 'home'
+      msg: 'home',
+      newsArr: []
+    }
+  },
+  filters: {
+    categoryFilters (type) {
+      let str = ''
+      switch (type) {
+        case 1:
+          str = '其他'
+          break
+        case 2:
+          str = '娱乐'
+          break
+        case 3:
+          str = '原创'
+          break
+        case 4:
+          str = '历史'
+          break
+        case 5:
+          str = '国际'
+          break
+      }
+      return str
+    },
+    dateFilters (timestamp) {
+      var date = new Date(timestamp * 1000)
+      var Y = date.getFullYear() + '-'
+      var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-'
+      var D = date.getDate() + ' '
+      var h = date.getHours() + ':'
+      var m = date.getMinutes() + ':'
+      var s = date.getSeconds()
+      return Y + M + D + h + m + s
     }
   },
   mounted () {
@@ -34,6 +64,7 @@ export default {
         url: 'http://restaurant.yijiahotel.shop/v1/news/1/10'
       }).then((res) => {
         console.log(res)
+        this.newsArr = res.data.data
       })
     })
   }
@@ -41,5 +72,43 @@ export default {
 </script>
 
 <style scoped>
-
+  .news{
+    /*height:2.45rem;*/
+    display:flex;
+    justify-content: center;
+    padding-top:0.2rem;
+    padding-bottom:0.2rem;
+    margin:0;
+    border-bottom:1px solid #ccc;
+  }
+  .news img{
+    width:2.4rem;
+    height:2rem;
+  }
+  .content{
+    margin-left:0.2rem;
+    margin-top:0.1rem;
+  }
+  .content-title{
+    color:black;
+    font-size:0.35rem;
+    margin-top:0.1rem;
+  }
+  .content-foot{
+    margin-top:0.1rem;
+  }
+  .content-foot span{
+    color:#7A7777;
+  }
+  .txt-type{
+    font-size:0.25rem;
+    border:1px solid #ccc;
+    padding:0.05rem;
+  }
+  .comments{
+    font-size:0.2rem;
+  }
+  .date{
+    font-size:0.2rem;
+  }
 </style>
